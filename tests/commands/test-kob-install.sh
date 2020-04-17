3d89079e820c14d9b6f89413883bcb96d4ecb358
Akshaydevsv21 <32606030+Akshaydevsv21@users.noreply.github.com>
2020-04-17 12:31:21 +0530

    Updated RF 1.0.1.3.2 - test-kob-install.sh
    Added functions kob_init,kob_execute,kob_verify,kob_cleanup
    Added TODO lists as comments.

--- tests/commands/test-kob-install.sh
+++ tests/commands/test-kob-install.sh
@@ -4,7 +4,7 @@ function kob_init {
 
   #TODO:- Initialization of KOBman environment variables.(e.g- KOB_DIR ).
   #TODO:- Set path_to_kob_env_tests(e.g - src/tests/envs), kob_environment_under_test, kob_test_namespace.
-  #TODO:- Check $kob command (e.g - $kob --version or $kob)
+  #TODO:- Check $kob command (e.g - $kob --version or $kob).
   #TODO:- Load function to test kob_environment_under_test(i.e source $path_to_kob_env_tests/test-$(kob_environment_under_test).sh).
   
 }
