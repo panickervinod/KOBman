9fef793c6862a7b5fa8999e170d5809a736c893c
Akshay Dev <akshaydevsv21@gmail.com>
2020-04-22 21:31:59 +0530

    First commit-Updated kob_init function

--- tests/commands/test-kob-install.sh
+++ tests/commands/test-kob-install.sh
@@ -3,30 +3,48 @@
 function kob_init {
 
   #TODO:- Initialization of KOBman environment variables.(e.g- KOB_DIR ).
-  #TODO:- Set path_to_kob_env_tests(e.g - src/tests/envs), kob_environment_under_test, kob_test_namespace.
-  #TODO:- Check $kob command (e.g - $kob --version or $kob).
-  #TODO:- Load function to test kob_environment_under_test(i.e source $path_to_kob_env_tests/test-$(kob_environment_under_test).sh).
+  KOBMAN_DIR="/home/user/.kobman"
   
+  #TODO:- Set path_to_kob_env_tests(e.g - src/tests/envs), kob_environment_under_test, kob_test_namespace.
+  path_to_kob_env_tests="${KOBMAN_DIR}/env"
+  kob_environment_under_test=$1 
+  kob_test_namespace=$2
+  
+  #TODO:- Check $kob command (e.g - $kob --version or $kob).
+  curl -L https://raw.githubusercontent.com/hyperledgerkochi/KOBman/master/get.kobman.io | bash > /dev/null 2>&1
+  source "/home/user/.kobman/bin/kobman-init.sh"
+  kob | grep -i "KOBman" >/dev/null
+  if [ $? -eq 0 ];
+        then echo "kob is installed" 
+  else
+        echo "kob not installed.."
+  fi
+   
+  #TODO:- Load function to test kob_environment_under_test(i.e source $path_to_kob_env_tests/test-$(kob_environment_under_test).sh).
+  function _test_kob_env {
+    source "$path_to_kob_env_tests/test-${kob_environment_under_test}.sh"
+  }
 }
 
-function kob_execute {
+#function kob_execute {
 
   #TODO:- kob install --environment ${ kob_environment_under_test } --namespace ${ kob_test_namespace }. ---> output file.
   #TODO:- Check output log file.
   
-}
+#}
 
-function kob_verify {   
+#function kob_verify {   
 
    #TODO:- Call function __test_kobman_$(environment_under_name)_ validate_install( optional arguments) --> output file.
    #TODO:- Check Output log file.
    
-}
+#}
 
-function kob_cleanup {
+#function kob_cleanup {
 
     #TODO:- Unset environment variables , delete any temp file written by the test.
     #TODO:- Remove function __test_kobman_$(environment_under_name)_ validate_install().
     
-}
+#}
 
+kob_init "$1" "$2"
