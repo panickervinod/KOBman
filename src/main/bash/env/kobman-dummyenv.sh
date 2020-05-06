feb4c8508d36980a0c2e8f359e8cc65bee4baa44
Vinod Panicker <panickervinod@users.noreply.github.com>
2020-05-06 09:22:55 +0530

    Update kobman-dummyenv.sh
    Add other functions that map to other commands in the dummy env.

--- src/main/bash/env/kobman-dummyenv.sh
+++ src/main/bash/env/kobman-dummyenv.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
 function __kobman_install_dummyenv {
-        echo "dummyenv installed"
+        echo "dummyenv install invoked"
 }
 
+
+function __kobman_uninstall_dummyenv {
+        echo "dummyenv uninstall invoked"
+}
+
+function __kobman_init_dummyenv {
+        echo "dummyenv init invoked"
+                
+}
+
+function __kobman_update_dummyenv {
+        echo "dummyenv update invoked"
+}
+
+function __kobman_reset_dummyenv {
+        echo "dummyenv reset invoked"
+}
+
+function __kobman_start_dummyenv {
+        echo "dummyenv start invoked"
+}
+
+function __kobman_stop_dummyenv {
+        echo "dummyenv stop invoked"
+}
 
 
