0e3d2454a8ce1ef426cf07d30c8d9ee029558a26
Arun Suresh <arunsureshampadath@gmail.com>
2020-06-10 21:00:33 +0530

    release-typo in namespace, publish-zip change, tmpl-code updation

--- bin/release.sh
+++ bin/release.sh
@@ -12,15 +12,17 @@ if [[ -z "$kob_version" ]];
 fi
 
 #setting up environment variables
-if [[ -z $KOB_ARCHIVE_DOWNLOAD_REPO ]];
-    then
-        KOB_ARCHIVE_DOWNLOAD_REPO={KOB_ARCHIVE_DOWNLOAD_REPO:-KOBman}
-fi
+# if [[ -z $KOB_ARCHIVE_DOWNLOAD_REPO ]];
+#     then
+#         KOB_ARCHIVE_DOWNLOAD_REPO={KOB_ARCHIVE_DOWNLOAD_REPO:-KOBman}
+# fi
 
-if [[ -z $KOB_NAMESPACE ]];
+if [[ -z $KOBMAN_NAMESPACE ]];
     then
-        KOB_NAMESPACE={KOB_NAMESPACE:-hyperledgerkochi}
-fi
+        
+        KOB_NAMESPACE={KOBMAN_NAMESPACE:-hyperledgerkochi}
+
+fi      
 
 # prepare branch
 cd ~/KOBman
@@ -35,7 +37,7 @@ cp ~/KOBman/scripts/tmpl/*.tmpl ~/KOBman/scripts/
 for file in ~/KOBman/scripts/*.tmpl;
 do
     sed -i "s/@KOB_VERSION@/$kob_version/g" $file
-    sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/$KOB_ARCHIVE_DOWNLOAD_REPO/g" $file
+    sed -i "s/@KOB_ARCHIVE_DOWNLOAD_REPO@/KOBman/g" $file
     sed -i "s/@KOB_NAMESPACE@/$KOB_NAMESPACE/g" $file
     # renaming to remove .tmpl extension
     mv "$file" "${file//.tmpl/}"
@@ -52,6 +54,6 @@ git push -f -u origin $branch
 git tag -a $kob_version -m "Releasing version $kob_version"
 git push origin $kob_version
 
-#checkout to dev
+#checkout to master
 git checkout master
 
